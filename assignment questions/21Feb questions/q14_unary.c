311f11cf0647c406cee9ef1f80446e0d80b679b9
silvrfur <silverfur03@gmail.com>
2023-02-21 23:09:26 +0530

    added school assignments folder

---
+++ assignment questions/21Feb questions/q14_unary.c
@@ -0,0 +1,11 @@
+//increment and decrement operators
+#include <stdio.h>
+
+int main()
+{
+    int a=6,b=4;
+    int ans=a++ +--b;
+    printf("%d",ans);
+
+    return 0;
+}
---
+++ assignment questions/operators.c
@@ -0,0 +1,68 @@
+// OPERATORS IN C
+#include <stdio.h>
+
+int main() {
+    int a = 9,b = 4/*assignment operator*/, c;
+    
+    //Arthematic operators
+    c = a+b;
+    printf("a+b = %d \n",c);
+    c = a-b;
+    printf("a-b = %d \n",c);
+    c = a*b;
+    printf("a*b = %d \n",c);
+    c = a/b;
+    printf("a/b = %d \n",c);
+    c = a%b;
+    printf("Remainder when a divided by b = %d \n",c);
+    
+    //unary operators
+    printf("++a = %d \n", ++a);
+    printf("--b = %d \n", --b);
+    
+    //relational operators
+    a = 5, b = 5, c = 10;
+
+    printf("%d == %d is %d \n", a, b, a == b);
+    printf("%d == %d is %d \n", a, c, a == c);
+    printf("%d > %d is %d \n", a, b, a > b);
+    printf("%d > %d is %d \n", a, c, a > c);
+    printf("%d < %d is %d \n", a, b, a < b);
+    printf("%d < %d is %d \n", a, c, a < c);
+    printf("%d != %d is %d \n", a, b, a != b);
+    printf("%d != %d is %d \n", a, c, a != c);
+    printf("%d >= %d is %d \n", a, b, a >= b);
+    printf("%d >= %d is %d \n", a, c, a >= c);
+    printf("%d <= %d is %d \n", a, b, a <= b);
+    printf("%d <= %d is %d \n", a, c, a <= c);
+    
+    
+    //logical operators
+    a = 5, b = 5, c = 10;
+    int result;
+
+    result = (a == b) && (c > b);
+    printf("(a == b) && (c > b) is %d \n", result);
+
+    result = (a == b) && (c < b);
+    printf("(a == b) && (c < b) is %d \n", result);
+
+    result = (a == b) || (c < b);
+    printf("(a == b) || (c < b) is %d \n", result);
+
+    result = (a != b) || (c < b);
+    printf("(a != b) || (c < b) is %d \n", result);
+
+    result = !(a != b);
+    printf("!(a != b) is %d \n", result);
+
+    result = !(a == b);
+    printf("!(a == b) is %d \n", result);
+    
+    //ternary
+    printf("enter age");
+    int age=scanf("%d",&age);
+    (age>=18)?printf("can vote"):printf("cant vote");
+
+    return 0;
+}
