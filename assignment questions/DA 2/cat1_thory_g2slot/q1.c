19013620f3a77c509c90f2fe080c6d14452ac5e0
silvrfur <111216714+silvrfur@users.noreply.github.com>
2023-05-02 10:39:44 +0530

    Update q1.c

--- assignment questions/DA 2/cat1_thory_g2slot/q1.c
+++ assignment questions/DA 2/cat1_thory_g2slot/q1.c
@@ -11,3 +11,41 @@ of 7 days and the temperature of Gangtok (
 0C) as output for the week of 7 days. 
 Implement the program using arrays and validate the rules.
 */
+#include <stdio.h>
+
+int main() {
+    float chennai_temps[7];
+    float delhi_temps[7];
+    float haveri_temps[7];
+    float gangtok_temps[7];
+
+    
+    printf("Enter the temperature of Chennai for each day of the week (in degrees Celsius):\n");
+    for (int i = 0; i < 7; i++) {
+        scanf("%f", &chennai_temps[i]);
+
+      
+        if (chennai_temps[i] < 28 || chennai_temps[i] > 33) {
+            printf("Invalid temperature! Temperature should be between 28C and 33C.\n");
+            return 0;
+        }
+    }
+
+    
+    for (int i = 0; i < 7; i++) {
+        delhi_temps[i] = chennai_temps[i] - 8;
+        haveri_temps[i] = chennai_temps[i] + 5;
+    }
+
+    for (int i = 0; i < 7; i++) {
+        gangtok_temps[i] = haveri_temps[i] - delhi_temps[i];
+    }
+
+    
+    printf("The temperature of Gangtok for each day of the week (in degrees Celsius):\n");
+    for (int i = 0; i < 7; i++) {
+        printf("%.2f\n", gangtok_temps[i]);
+    }
+
+    return 0;
+}
