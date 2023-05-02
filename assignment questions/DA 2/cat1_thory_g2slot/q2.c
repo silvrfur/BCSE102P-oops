4a2d6d2d3257f48d31b41811546fe7b48e36cbd5
silvrfur <111216714+silvrfur@users.noreply.github.com>
2023-05-02 10:41:27 +0530

    Update q2.c

--- assignment questions/DA 2/cat1_thory_g2slot/q2.c
+++ assignment questions/DA 2/cat1_thory_g2slot/q2.c
@@ -11,3 +11,36 @@ of 7 days and the temperature of Gangtok (
 0C) as output for the week of 7 days. 
 Implement the program using arrays and validate the rules.
 */
+#include <stdio.h>
+
+
+int sumOfDigits(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int sum = 0;
+    
+    
+    for (int i = 1000; i <= 9998; i += 2) {
+        sum += i;
+    }
+    
+    
+    while (sum > 9) {
+        sum = sumOfDigits(sum);
+    }
+  
+    if (sum % 2 == 0) {
+        printf("Even found\n");
+    } else {
+        printf("Odd found\n");
+    }
+    
+    return 0;
+}
