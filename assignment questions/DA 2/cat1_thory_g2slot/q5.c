f72bbf38b90fe5008c6638a5ad378d327b47bdc8
silvrfur <111216714+silvrfur@users.noreply.github.com>
2023-05-02 10:43:15 +0530

    Update q5.c

--- assignment questions/DA 2/cat1_thory_g2slot/q5.c
+++ assignment questions/DA 2/cat1_thory_g2slot/q5.c
@@ -4,3 +4,29 @@ Using the concept of recursion, help Paul to write a “C program” for finding
 of (x)n
  where n should be less than or equal to 5
 */
+#include <stdio.h>
+
+int power(int x, int n);
+
+int main() {
+    int x, n, result;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    printf("Enter the power (<= 5): ");
+    scanf("%d", &n);
+    if(n > 5) {
+        printf("Error: Power should be less than or equal to 5.");
+        return 0;
+    }
+    result = power(x, n);
+    printf("%d raised to the power %d is %d\n", x, n, result);
+    return 0;
+}
+
+int power(int x, int n) {
+    if(n == 0) {
+        return 1;
+    } else {
+        return x * power(x, n-1);
+    }
+}
